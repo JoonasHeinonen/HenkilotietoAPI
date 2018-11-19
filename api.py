cdece311869a3107a2705754eb3698bdfca8f48a
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2018-11-19 18:45:31 +0200

    Created an array for personal data of a person and method to display it.

--- api.py
+++ api.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+personalData = [{'First Name' : 'Matti', 'Last Name' : 'Meikalainen',
+            'Phone Number' : '044 3064321', 'Home Address' : 'Matintie 3',
+            'Municipality': 'Turku', 'Social Security Number' : '010100-333I'}]
+
+@app.route("/", methods=['GET'])
+def printData():
+    return jsonify({'Personal Data' : personalData})
 
 if __name__ == '__main__':
     app.run(debug=True)
