7bcf6e0ce232a4931b2890ec19ed9d1ac233fd10
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2018-11-19 19:09:06 +0200

    Saved file from the home  computer and updated the array.

--- api.py
+++ api.py
@@ -2,12 +2,12 @@ from flask import Flask, request
 from flask import jsonify
 app = Flask(__name__)
 
-personalData = [{'First Name' : 'Matti', 
-            'Last Name' : 'Meikalainen',
-            'Phone Number' : '044 3064321', 
-            'Home Address' : 'Matintie 3',
-            'Municipality': 'Turku', 
-            'Social Security Number' : '010100-333I'}]
+personalData = [{'First Name' : 'Matti'}, 
+            {'Last Name' : 'Meikalainen'},
+            {'Phone Number' : '044 3064321'}, 
+            {'Home Address' : 'Matintie 3'},
+            {'Municipality': 'Turku'}, 
+            {'Social Security Number' : '010100-333I'}]
 
 @app.route("/printdata", methods=['GET'])
 def printData():
