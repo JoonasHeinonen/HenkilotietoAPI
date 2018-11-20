90142a9a4ac3a3dfb38c0e9438497c0220e08a97
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2018-11-20 17:20:13 +0200

    Modified the method so that it will only display the first book.

--- api.py
+++ api.py
@@ -18,8 +18,11 @@ books = [
 ]
 
 @app.route("/printdata", methods=['GET'])
-def get_books():
-    return jsonify({'books': books})
+def get_books(book_id):
+    book = [book for book in books if book['id'] == book_id]
+    if len(book) == 0:
+        abort(404)
+    return jsonify({'books': book[0]})
 
 if __name__ == '__main__':
     app.run(debug=True)
