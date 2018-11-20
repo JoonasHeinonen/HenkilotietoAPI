0d08b46676734da7c4be75f3116dd6dece8d504d
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2018-11-20 17:32:47 +0200

    Imported make_response and added an error handler.

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
-from flask import Flask, request
+from flask import Flask
+from flask import request
 from flask import abort
 from flask import jsonify
+from flask import make_response
 app = Flask(__name__)
 
 books = [
@@ -24,6 +26,10 @@ def get_books(book_id):
     if len(book) == 0:
         abort(404)
     return jsonify({'book': book[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     app.run(debug=True)
