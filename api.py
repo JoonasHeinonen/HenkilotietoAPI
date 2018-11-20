25deba0c78558f5e8ca8d28c947068c1f84b759b
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2018-11-20 16:42:05 +0200

    Changed method and the Json data.

--- api.py
+++ api.py
@@ -2,16 +2,24 @@ from flask import Flask, request
 from flask import jsonify
 app = Flask(__name__)
 
-personalData = [{'First Name' : 'Matti'}, 
-            {'Last Name' : 'Meikalainen'},
-            {'Phone Number' : '044 3064321'}, 
-            {'Home Address' : 'Matintie 3'},
-            {'Municipality': 'Turku'}, 
-            {'Social Security Number' : '010100-333I'}]
+tasks = [
+    {
+        'id': 1,
+        'title': u'The Origins Of Freemasonry',
+        'description': 'Can the ancestry of freemasonry really be...',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Communist Manifesto',
+        'description': 'Classic of political thinking from 1848 is...',
+        'done': False
+    }
+]
 
 @app.route("/printdata", methods=['GET'])
-def printData():
-    return jsonify({'Personal Data' : personalData})
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 if __name__ == '__main__':
     app.run(debug=True)
