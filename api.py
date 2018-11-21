ef5de79b477219fee2d52567d3d0bb826d416716
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2018-11-21 21:38:47 +0200

    Added PUT and DELETE methods.

--- api.py
+++ api.py
@@ -45,6 +45,62 @@ def get_books(book_id):
         abort(404)
     return jsonify({'book': book[0]})
 
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_book():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    book = {
+        'title': request.json['title'],
+        'description': request.json['description'],
+        'keyword': request.json['keyword'],
+        'modified': datetime.datetime.now(),
+        'publisher': request.json['publisher'],
+        'contact': request.json['contact'],
+        'mbox': request.json['mbox'],
+        'id': books[-1]['id'] + 1,
+        'access': request.json['access']
+    }
+    books.append(book)
+    return jsonify({'book': book}), 201
+
+@app.route('/todo/api/v1.0/tasks/<int:book_id>', methods=['PUT'])
+def update_book(book_id):
+    book = [book for book in books if book['id'] == book_id]
+    if len(book) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) != unicode:
+        abort(400)
+    if 'keyword' in request.json and type(request.json['keyword']) != unicode:
+        abort(400)
+    if 'publisher' in request.json and type(request.json['publisher']) != unicode:
+        abort(400)
+    if 'contact' in request.json and type(request.json['contact']) != unicode:
+        abort(400)
+    if 'mbox' in request.json and type(request.json['mbox']) != unicode:
+        abort(400)
+    if 'access' in request.json and type(request.json['access']) != unicode:
+        abort(400)
+    book[0]['title'] = request.json.get('title', book[0]['title'])
+    book[0]['description'] = request.json.get('description', book[0]['description'])
+    book[0]['keyword'] = request.json.get('keyword', book[0]['keyword'])
+    book[0]['publisher'] = request.json.get('publisher', book[0]['publisher'])
+    book[0]['contact'] = request.json.get('contact', book[0]['contact'])
+    book[0]['mbox'] = request.json.get('mbox', book[0]['mbox'])
+    book[0]['access'] = request.json.get('access', book[0]['access'])
+    return jsonify({'book': book[0]})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_book(book_id):
+    book = [book for book in books if book['id'] == book_id]
+    if len(book) == 0:
+        abort(404)
+    books.remove(book[0])
+    return jsonify({'result': True})
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
